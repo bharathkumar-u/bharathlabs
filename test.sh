733edafcea170cb6f9c38426fcdf732d331d1638
bharathkumar-u <41358496+bharathkumar-u@users.noreply.github.com>
2018-11-29 20:07:30 +0530

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Enter your first name: " a
+read -p "Enter your second name: " b
+echo "$a$b"
+if [ "$a$b" == bharathkumar ]; then 
+/Users/bharathkumar/./test1.sh
+fi
---
+++ test1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+age=30
+echo " Age of the person is $age"
